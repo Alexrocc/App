45633f6422bfedf6ca309b0406cb3ad3129ba111
Alexrocc <alerocc70@gmail.com>
2024-07-31 19:08:11 +0200

    update data

--- DapperCRUD.cs
+++ DapperCRUD.cs
@@ -1,26 +1,26 @@
-
-
 using System.Data;
 using System.Data.SqlClient;
 using Dapper;
-using Microsoft.VisualBasic;
 
-class DapperCrud()
+class DapperCrud
 {
     static readonly string connString = """Server=DESKTOP-5NUP6OJ\SQLEXPRESS; User = sa; Password = sa; Database=PMSAPI;""";
     public static void Main()
     {
-        foreach (var project in ReadData(10, connString))
+        // var id = AddData(connString, "C# Project", "Project made with C# and Dapper", DateTime.Now, DateTime.Now.AddDays(30));
+        var id = ModifyData(connString, 5, "C# Project Updated", "Project updated with C# and Dapper", DateTime.Now, DateTime.Now.AddDays(30));
+        Console.WriteLine(id);
+        foreach (var project in ReadData(0, 30, connString))
         {
             Console.WriteLine($"{project.Name}");
         }
     }
 
-    public static List<Project> ReadData(int limit, string conn)
+    public static List<Project> ReadData(int offset, int limit, string conn)
     {
+        // List<Project> fetch = new();
         try
         {
-
             using (IDbConnection db = new SqlConnection(conn))
             {
                 string query = """
@@ -29,10 +29,10 @@ class DapperCrud()
                 category_id as CategoryId, manager_id as ManagerID 
                 FROM projects 
                 ORDER BY id
-                OFFSET @limit ROWS
+                OFFSET @offset ROWS
                 FETCH NEXT @limit ROWS ONLY
             """;
-                return db.Query<Project>(query, new { limit }).ToList();
+                return db.Query<Project>(query, new { offset, limit }).ToList();
             }
         }
         catch (Exception ex)
@@ -40,19 +40,63 @@ class DapperCrud()
             Console.WriteLine(ex);
             return new List<Project>();
         }
+        // return fetch;
     }
 
-    public void AddData()
+    public static long AddData(string conn, string name, string desc, DateTime start, DateTime end)
     {
-
+        long lastId = 0;
+        try
+        {
+            string query = """
+        INSERT INTO projects (name, description, start_date, end_date)
+        VALUES (@name, @description, @start_date, @end_date)
+        SELECT SCOPE_IDENTITY();
+        """;
+            using (IDbConnection db = new SqlConnection(conn))
+            {
+                // var result = db.Execute(query, new { projName = name, description = desc, start_date = start, end_date = end });
+                lastId = db.QuerySingle<long>(query, new { name, description = desc, start_date = start, end_date = end }); //returns the last inserted id
+            }
+        }
+        catch (SqlException ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        return lastId;
     }
 
-    public void ModifyData()
+    public static int ModifyData(string conn, int id, string name, string desc, DateTime start, DateTime end)
     {
-
+        int result = 0;
+        try
+        {
+            string query = """
+        UPDATE projects SET name = @projName, description = @desc, start_date = @start, end_date = @end
+        WHERE id = @id        
+        """;
+            using (IDbConnection db = new SqlConnection(conn))
+            {
+                result = db.Execute(query, new { id, projName = name, desc, start, end });
+                // db.QuerySingle<long>(query, new { id, projName = name, description = desc, start_date = start, end_date = end }); //returns the last inserted id
+            }
+        }
+        catch (SqlException ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        return result;
     }
 
-    public void DeleteData()
+    public static void DeleteData()
     {
 
     }
--- obj/Debug/net8.0/App.AssemblyInfo.cs
+++ obj/Debug/net8.0/App.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f2b82833f80bc3f4b83e730603715b4da9fb36de")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e8d964313000a27bb6f19b03c9262df33e31b000")]
 [assembly: System.Reflection.AssemblyProductAttribute("App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
